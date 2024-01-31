5c5a6f1241719000377b3ec4c053c8f9b64f3a0b
Yuri <yuri_henrick@hotmail.com>
2024-01-31 10:32:17 -0300

    Criação do CRUD Produto e relacionamento com categoria

--- src/main/java/com/generation/farmacia/model/Produto.java
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -1,46 +1,51 @@
 package com.generation.farmacia.model;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.List;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.generation.farmacia.model.Categoria;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 @Entity //indica que essa classe no meu Java é uma Entidade ou seja um tabela no meu banco de dados... para diferenciar de uma classe normal, feita com JPA e hibernate
-@Table(name = "tb_categorias")
-public class Categoria {
+@Table(name = "tb_produtos")
+public class Produto {
 
 	@Id // indica que vai ser minha chave primária da minha tabela
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // vai gerar minha chave primária com Autoincrement no meu banco de dados
 	private Long id;
 	
-	@NotBlank(message= "O atributo tipo é de prenchimento obrigatório") // Not null no banco de dados
-	@Size(min = 5, max = 100, message = "O atributo tipo tem no mínimo 5 caracteres e no máximo 100 caracteres.")
-	private String tipo;
+	@NotBlank(message= "O atributo nome é de prenchimento obrigatório") // Not null no banco de dados
+	@Size(min = 3, max = 100, message = "O atributo nome tem no mínimo 5 caracteres e no máximo 100 caracteres.")
+	private String nome;
 	
 	@NotBlank(message= "O atributo descricao é de prenchimento obrigatório") // Not null no banco de dados
 	@Size(min = 10, max = 1000, message = "O atributo descricao tem no mínimo 10 caracteres e no máximo 1000 caracteres.")
 	private String descricao;
 	
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+	@NotNull(message = "Preço é obrigatório!")
+	@Positive(message = "O preço deve ser maior do que zero!")
+	private BigDecimal preco;
+	
 	@UpdateTimestamp
 	private LocalDateTime data;
 	
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
-	@JsonIgnoreProperties("categoria")
-	private List<Produto> produto;
-	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
 
 	public Long getId() {
 		return id;
@@ -50,12 +55,12 @@ public class Categoria {
 		this.id = id;
 	}
 
-	public String getTipo() {
-		return tipo;
+	public String getNome() {
+		return nome;
 	}
 
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 	public String getDescricao() {
@@ -74,14 +79,13 @@ public class Categoria {
 		this.data = data;
 	}
 
-	public List<Produto> getProduto() {
-		return produto;
+	public Categoria getCategoria() {
+		return categoria;
 	}
 
-	public void setProduto(List<Produto> produto) {
-		this.produto = produto;
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
 	}
-
 	
 	
 }
